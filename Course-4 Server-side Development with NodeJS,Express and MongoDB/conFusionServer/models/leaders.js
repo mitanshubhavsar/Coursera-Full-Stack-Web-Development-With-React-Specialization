e672450de5f8206a58deb9e8508fcb294666541d
mitanshubhavsar <64788813+mitanshubhavsar@users.noreply.github.com>
2020-09-21 21:42:02 +0530

    Add files via upload

---
+++ Course-4 Server-side Development with NodeJS,Express and MongoDB/conFusionServer/models/leaders.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+require('mongoose-currency').loadType(mongoose);
+const Currency = mongoose.Types.Currency;
+
+var feedbackSchema = new Schema({
+    rating:  {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true
+    },
+    feedback:  {
+        type: String,
+        required: true
+    },
+    author:  {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+var leaderSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    designation: {
+        type: String,
+        required: true
+    },
+    abbr: {
+        type: String,
+        required: true
+    },
+    featured: {
+        type: Boolean,
+        default:false      
+    },
+    feedback:[feedbackSchema]
+}, {
+    timestamps: true
+});
+
+var Leaders = mongoose.model('Leader', leaderSchema);
+
+module.exports = Leaders;
